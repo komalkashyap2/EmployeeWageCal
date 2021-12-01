a1d32c7d2ee76bfaebcb6075713bb0c7bd0213ff
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-01 16:58:57 +0530

    [KOMALKASHYAP] Add : added Employee part time

--- EmployeeWageCalculation/EmployeeWageCalculation/EmployeeWage.cs
+++ EmployeeWageCalculation/EmployeeWageCalculation/EmployeeWage.cs
@@ -7,9 +7,10 @@ using System.Threading.Tasks;
 namespace EmployeeWageCalculation
 {
      public class EmployeeWage
-    {
+     {
         //constant
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         //variables
         int empHrs = 0;
@@ -47,5 +48,28 @@ namespace EmployeeWageCalculation
 
         }
 
-     }
+        public void EmployeePartTime()
+        {
+            //computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage:" + empWage);
+
+
+        }
+    }
 }
