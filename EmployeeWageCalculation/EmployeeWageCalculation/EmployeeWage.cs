5119505d4c1cdc4808669669b7bcee6c662ce2a3
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-01 15:50:05 +0530

    [KOMALKASHYAP] Add : added Attendance method

---
+++ EmployeeWageCalculation/EmployeeWageCalculation/EmployeeWage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCalculation
+{
+     public class EmployeeWage
+    {
+        //constant
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        public void Attendance()
+        {
+            //computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("employee1 is present");
+
+            }
+            else
+            {
+                Console.WriteLine("employee1 is present");
+            }
+        }
+
+    }
+}
