8e9635d43aa99eabbad25bb760f21b297d4d1a11
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-01 19:48:03 +0530

    [KOMALKASHYAP] Add : added Employee part or full time

--- EmployeeWageCalculation/EmployeeWageCalculation/EmployeeWage.cs
+++ EmployeeWageCalculation/EmployeeWageCalculation/EmployeeWage.cs
@@ -9,9 +9,9 @@ namespace EmployeeWageCalculation
      public class EmployeeWage
      {
         //constant
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         //variables
         int empHrs = 0;
         int empWage = 0;
@@ -47,8 +47,8 @@ namespace EmployeeWageCalculation
             
 
         }
-
         public void EmployeePartTime()
+
         {
             //computation
             int empCheck = random.Next(0, 3);
@@ -71,5 +71,30 @@ namespace EmployeeWageCalculation
 
 
         }
+        public void PartOrFullTIme()
+        {
+             
+            //computation
+            int empCheck = random.Next(0, 3);
+
+            switch (empCheck)
+
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("EmpWage" + empWage);
+        }
+
     }
 }
